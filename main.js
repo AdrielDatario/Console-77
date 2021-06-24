5fcfbd43b9565ca424ac0dbb2cdd408567e18fbe
AdrielDatario <79733895+AdrielDatario@users.noreply.github.com>
2021-06-23 18:59:21 -0700

    remove commas, accessing using for loop

--- main.js
+++ main.js
@@ -1,27 +1,58 @@
-var name_of_array = [];
+var name_array = [];
 
 function submit()
 {
-    var n1 = document.getElementById("std1").value;
-    var n2 = document.getElementById("std2").value;
-    var n3 = document.getElementById("std3").value;
-    var n4 = document.getElementById("std4").value;
+    var display_student_array = [];
+    
+   
+    for (var j = 1; j <=4; j++){
+        var Names = document.getElementById("name_of_the_student_"+j).value;
+        console.log(Names);
+         name_array.push(Names);
+    }
 
-    name_of_array.push(n1);
-    name_of_array.push(n2);
-    name_of_array.push(n3);
-    name_of_array.push(n4);
+    console.log(name_array);
 
-    console.log(name_of_array);
+    var length_array = name_array.length;
+    console.log(length_array);
 
-    document.getElementById("display_name").innerHTML = name_of_array;
-    document.getElementById("sumit_button").style.display = "none";
+    for (var k = 0; k < length_array; k++){
+
+        display_student_array.push("<h4>NAME - " + name_array[k]+ "</h4>");
+        console.log(display_student_array);
+    }
+
+    console.log(display_student_array);
+    document.getElementById("display_name_with_commas").innerHTML = display_student_array;
+
+    var remove_commas = display_student_array.join(" ");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+
+
+    document.getElementById("submit_button").style.display = "none";
     document.getElementById("sort_button").style.display = "inline-block";
 }
 
-function sorting()
-{
-    name_of_array.sort();
-    console.log(name_of_array);
-    document.getElementById("display_name").innerHTML = name_of_array;
+function sorting(){
+
+    var hi = name_array.sort();
+    console.log(name_array);
+    console.log(hi);
+
+    var sorting_array = [];
+
+    var length_array = name_array.length;
+    console.log(length_array);
+
+    for (var k = 0; k < length_array; k++){
+
+        sorting_array.push("<h4>NAME - " + name_array[k]+ "</h4>");
+        console.log(sorting_array);
+    }
+
+    var remove_commas = sorting_array.join(" ");
+    console.log(remove_commas);
+    document.getElementById("display_name_without_commas").innerHTML = remove_commas;
+
 }
